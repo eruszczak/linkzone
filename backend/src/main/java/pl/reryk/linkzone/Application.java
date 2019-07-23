2f792d4777763129e1cc60951c0d800718c13249
eryk <me@eryk.io>
2019-07-23 14:07:14 +0200

    Update.

--- backend/src/main/java/pl/reryk/linkzone/Application.java
+++ backend/src/main/java/pl/reryk/linkzone/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.reryk.linkzone.config.FileStorageProperties;
 import pl.reryk.linkzone.dto.AccountCreate;
 import pl.reryk.linkzone.model.Account;
@@ -26,11 +24,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
