fc2f64474427d7b2e792dc037139097b85f2f9f6
eryk <me@eryk.io>
2020-11-08 18:03:09 +0100

    Prepare docker configuration

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,16 +2,11 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-            target: 'http://localhost:8080',
-            // secure: false,
-            // ws: true,
+            target: process.env.PROXY_TARGET || 'http://localhost:8080',
             changeOrigin: true
         },
         '/static': {
-            target: 'http://localhost:8080',
-          // pathRewrite: {
-          //   '^/static': ''
-          // },
+            target: process.env.PROXY_TARGET || 'http://localhost:8080',
           changeOrigin: true
       }
       },
@@ -19,31 +14,9 @@ module.exports = {
     },
     css: {
   loaderOptions: {
-    // pass options to sass-loader
     sass: {
-      // @/ is an alias to src/
-      // so this assumes you have a file named `src/variables.scss`
-      // data: `@import "~bourbon"`
     }
   }
 }
-  // module: {
-  //   loaders: [{
-  //     test: /\.js$/,
-  //     loader: 'babel',
-  //     exclude: /node_modules/
-  //   }, {
-  //     test: /\.vue$/,
-  //     loader: 'vue'
-  //   }, {
-  //     test: /\.s[a|c]ss$/,
-  //     loader: 'style!css!sass'
-  //   }]
-  // },
-  // vue: {
-  //   loaders: {
-  //     scss: 'style!css!sass'
-  //   }
-  // }
 };
 
